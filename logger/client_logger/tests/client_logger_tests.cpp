7adbc7b87fd59684fe02572abe89e8e93e839908
kristyavodore <vodorezova05@bk.ru>
2024-10-20 00:42:58 +0300

    example test

--- logger/client_logger/tests/client_logger_tests.cpp
+++ logger/client_logger/tests/client_logger_tests.cpp
@@ -1,12 +1,20 @@
 #include <gtest/gtest.h>
+#include "client_logger_builder.h"
 
 
+int main(){
+    std::cout << "AAAAAAAA" << std::endl;
+    auto *build_logger_1 = new client_logger_builder;
+    build_logger_1 -> add_file_stream("1.txt",logger::severity::information);
+    build_logger_1 -> set_format("%t %d %m %s");
 
-int main(
-    int argc,
-    char *argv[])
-{
-    testing::InitGoogleTest(&argc, argv);
+    logger * logger_1 = build_logger_1 -> build();
 
-    return RUN_ALL_TESTS();
+    logger_1 -> log("lalalalalaaaaaa", logger::severity::information);
+
+    delete build_logger_1;
+    delete logger_1;
+
+    std::cout << sizeof(unsigned char*)*300;
+    return 0;
 }
